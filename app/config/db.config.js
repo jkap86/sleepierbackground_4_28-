3372a933cec6b3b81b9ae55d559f8cdbf400cebc
Jay Kapoor <jkap86@gmail.com>
2023-05-02 14:54:49 -0400

    -

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    DATABASE_URL: process.env.DATABASE_URL || 'postgres://postgres:password123@localhost:5432/sleepier'
+    DATABASE_URL: process.env.DATABASE_URL || 'postgres://postgres:password123@localhost:5432/postgres'
 };
--- app/controllers/sync.controller.js
+++ app/controllers/sync.controller.js
@@ -105,110 +105,115 @@ exports.trades = async (app) => {
 
 
         let trades_league = []
-        await Promise.all(leagues_to_update
-            .filter(x => x.dataValues.rosters.find(r => r.players?.length > 0))
-            .map(async league => {
 
-                let transactions_league;
+        const batch_size = 50
 
-                try {
-                    transactions_league = await axios.get(`https://api.sleeper.app/v1/league/${league.dataValues.league_id}/transactions/${state.season_type === 'regular' ? state.week : 1}`)
-                } catch (error) {
-                    console.log(error)
-                    transactions_league.data = []
-                }
+        for (let i = 0; i < leagues_to_update.length; i += batch_size) {
+            await Promise.all(leagues_to_update
+                .filter(x => x.dataValues.rosters.find(r => r.players?.length > 0))
+                .map(async league => {
 
-                try {
-                    transactions_league.data
-                        .map(transaction => {
-                            const draft_order = league.dataValues.drafts.find(d => d.draft_order && d.status !== 'complete')?.draft_order
+                    let transactions_league;
 
-                            const managers = transaction.roster_ids.map(roster_id => {
-                                const user = league.dataValues.rosters?.find(x => x.roster_id === roster_id)
+                    try {
+                        transactions_league = await axios.get(`https://api.sleeper.app/v1/league/${league.dataValues.league_id}/transactions/${state.season_type === 'regular' ? state.week : 1}`)
+                    } catch (error) {
+                        console.log(error)
+                        transactions_league.data = []
+                    }
 
-                                return user?.user_id
-                            })
+                    try {
+                        transactions_league.data
+                            .map(transaction => {
+                                const draft_order = league.dataValues.drafts.find(d => d.draft_order && d.status !== 'complete')?.draft_order
 
-                            const draft_picks = transaction.draft_picks.map(pick => {
-                                const roster = league.dataValues.rosters.find(x => x.roster_id === pick.roster_id)
-                                const new_roster = league.dataValues.rosters.find(x => x.roster_id === pick.owner_id)
-                                const old_roster = league.dataValues.rosters.find(x => x.roster_id === pick.previous_owner_id)
+                                const managers = transaction.roster_ids.map(roster_id => {
+                                    const user = league.dataValues.rosters?.find(x => x.roster_id === roster_id)
 
-                                return {
-                                    ...pick,
-                                    original_user: {
-                                        user_id: roster?.user_id,
-                                        username: roster?.username,
-                                        avatar: roster?.avatar,
-                                    },
-                                    new_user: {
-                                        user_id: new_roster?.user_id,
-                                        username: new_roster?.username,
-                                        avatar: new_roster?.avatar,
-                                    },
-                                    old_user: {
-                                        user_id: old_roster?.user_id,
-                                        username: old_roster?.username,
-                                        avatar: old_roster?.avatar,
-                                    },
-                                    order: draft_order && roster?.user_id && pick.season === state.league_season ? draft_order[roster?.user_id] : null
-                                }
-                            })
-
-                            let adds = {}
-                            transaction.adds && Object.keys(transaction.adds).map(add => {
-                                const user = league.dataValues.rosters?.find(x => x.roster_id === transaction.adds[add])
-                                return adds[add] = user?.user_id
-                            })
-
-                            let drops = {}
-                            transaction.drops && Object.keys(transaction.drops).map(drop => {
-                                const user = league.dataValues.rosters?.find(x => x.roster_id === transaction.drops[drop])
-                                return drops[drop] = user?.user_id
-                            })
-
-                            const pricecheck = []
-                            managers.map(user_id => {
-                                const count = Object.keys(adds).filter(a => adds[a] === user_id).length
-                                    + draft_picks.filter(pick => pick.new_user.user_id === user_id).length
-
-                                if (count === 1) {
-                                    const player = Object.keys(adds).find(a => adds[a] === user_id)
-                                    if (player) {
-                                        pricecheck.push(player)
-                                    } else {
-                                        const pick = draft_picks.find(pick => pick.new_user.user_id === user_id)
-                                        pricecheck.push(`${pick.season} ${pick.round}.${pick.order}`)
-                                    }
-                                }
-                            })
-
-
-                            if (transaction.type === 'trade') {
-
-                                trades_league.push({
-                                    transaction_id: transaction.transaction_id,
-                                    leagueLeagueId: league.dataValues.league_id,
-                                    status_updated: transaction.status_updated,
-                                    rosters: league.dataValues.rosters,
-                                    managers: managers,
-                                    players: [...Object.keys(adds), ...draft_picks.map(pick => `${pick.season} ${pick.round}.${pick.order}`)],
-                                    adds: adds,
-                                    drops: drops,
-                                    draft_picks: draft_picks,
-                                    drafts: league.dataValues.drafts,
-                                    price_check: pricecheck
+                                    return user?.user_id
                                 })
-                            }
 
-                        })
+                                const draft_picks = transaction.draft_picks.map(pick => {
+                                    const roster = league.dataValues.rosters.find(x => x.roster_id === pick.roster_id)
+                                    const new_roster = league.dataValues.rosters.find(x => x.roster_id === pick.owner_id)
+                                    const old_roster = league.dataValues.rosters.find(x => x.roster_id === pick.previous_owner_id)
 
-                } catch (error) {
-                    console.log(error)
-                }
+                                    return {
+                                        ...pick,
+                                        original_user: {
+                                            user_id: roster?.user_id,
+                                            username: roster?.username,
+                                            avatar: roster?.avatar,
+                                        },
+                                        new_user: {
+                                            user_id: new_roster?.user_id,
+                                            username: new_roster?.username,
+                                            avatar: new_roster?.avatar,
+                                        },
+                                        old_user: {
+                                            user_id: old_roster?.user_id,
+                                            username: old_roster?.username,
+                                            avatar: old_roster?.avatar,
+                                        },
+                                        order: draft_order && roster?.user_id && pick.season === state.league_season ? draft_order[roster?.user_id] : null
+                                    }
+                                })
+
+                                let adds = {}
+                                transaction.adds && Object.keys(transaction.adds).map(add => {
+                                    const user = league.dataValues.rosters?.find(x => x.roster_id === transaction.adds[add])
+                                    return adds[add] = user?.user_id
+                                })
+
+                                let drops = {}
+                                transaction.drops && Object.keys(transaction.drops).map(drop => {
+                                    const user = league.dataValues.rosters?.find(x => x.roster_id === transaction.drops[drop])
+                                    return drops[drop] = user?.user_id
+                                })
+
+                                const pricecheck = []
+                                managers.map(user_id => {
+                                    const count = Object.keys(adds).filter(a => adds[a] === user_id).length
+                                        + draft_picks.filter(pick => pick.new_user.user_id === user_id).length
+
+                                    if (count === 1) {
+                                        const player = Object.keys(adds).find(a => adds[a] === user_id)
+                                        if (player) {
+                                            pricecheck.push(player)
+                                        } else {
+                                            const pick = draft_picks.find(pick => pick.new_user.user_id === user_id)
+                                            pricecheck.push(`${pick.season} ${pick.round}.${pick.order}`)
+                                        }
+                                    }
+                                })
 
 
-            }))
+                                if (transaction.type === 'trade') {
+
+                                    trades_league.push({
+                                        transaction_id: transaction.transaction_id,
+                                        leagueLeagueId: league.dataValues.league_id,
+                                        status_updated: transaction.status_updated,
+                                        rosters: league.dataValues.rosters,
+                                        managers: managers,
+                                        players: [...Object.keys(adds), ...draft_picks.map(pick => `${pick.season} ${pick.round}.${pick.order}`)],
+                                        adds: adds,
+                                        drops: drops,
+                                        draft_picks: draft_picks,
+                                        drafts: league.dataValues.drafts,
+                                        price_check: pricecheck
+                                    })
+                                }
+
+                            })
+
+                    } catch (error) {
+                        console.log(error)
+                    }
+
+
+                }))
+        }
 
         try {
             await Trade.bulkCreate(trades_league, { ignoreDuplicates: true, returning: false })
